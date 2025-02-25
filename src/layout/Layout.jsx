b7d2bdb0762922d12776455177a528af95c44e4c
iqblmp <iqbalmauludipratama019@gmail.com>
2025-02-25 17:10:18 +0700

    fix building error

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -16,7 +16,7 @@ function Layout({ children, showWelcome }) {
                     </span>
                 </footer>
             )}
-            <TailwindIndicator />
+            {/* <TailwindIndicator /> */}
         </div>
     );
 }
